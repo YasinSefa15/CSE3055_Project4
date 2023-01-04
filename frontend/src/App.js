be7439bdd637f1804279580d6a5fbe9292f9771f
YasinSefa15 <yasekirman@gmail.com>
2023-01-04 04:34:04 +0300

    frontend skeleton created with bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import bootstrap from "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-danger">Merhaba!</button>
+    </div>
+  );
+}
+
+export default App;
